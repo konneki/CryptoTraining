dc3954244fbb9fb6c1219a49e9616f35ff637488
Konneki <konnekdaniel@gmail.com>
2023-07-18 20:46:41 +0200

    Initial Commit

---
+++ CryptoTraining/CryptoTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrainingApp.swift
+//  CryptoTraining
+//
+//  Created by Daniel Konnek on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
